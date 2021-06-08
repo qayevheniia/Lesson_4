c7a4c1f69455ae3c6889bd40d1e3dd6bfc263cc2
Yevgeniia Motytskaya <qayevheniia@gmail.com>
2021-06-09 00:26:26 +0300

    HW and additional task

--- src/com/mysite/Main.java
+++ src/com/mysite/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // wprintd all the values divisible by 7 in the range of 1 to 100 in reverse order (for)
+        // printd all the values divisible by 7 in the range of 1 to 100 in reverse order (for)
         // int i = 0;
 
         // do {
@@ -24,16 +24,16 @@ public class Main {
 //        int expected = 13;
 //        boolean result = false;
 //
-//       for (int i=0; i < array.length; i++)
-//        {
-//            if (array[i]==expected) {
-//                result = true;
-//                System.out.println (result) ;
-//                break;
+//         {
+////            if (array[i]==expected) {
+////                result = true;
+////                System.out.println (result) ;
+////                break;
+////
+////            }
+////            System.out.println(result);
+////       }   for (int i=0; i < array.length; i++)
 //
-//            }
-//            System.out.println(result);
-//       }
 //
 //        int height = 2;
 //        int width = 5;
@@ -85,8 +85,43 @@ public class Main {
 
     }
 
+    class Additional_task {
+        public static void main(String[] args) {
+
+//            1. Program to print the duplicate elements of an array
+//            In this program, we need to print the duplicate elements present in the array.
+//            This can be done through two loops.
+//            The first loop will select an element and the second loop will iteration through the array
+//            by comparing the selected element with other elements.
+//            If a match is found, print the duplicate element.
+//
+//                    Program to print the duplicate elements of an array
+//            In the above array, the first duplicate will be found at the index 4 which is the duplicate of the element (2) present at index 1. So, duplicate elements in the above array are 2, 3 and 8.
+//
+//            Algorithm
+//            STEP 1: START
+//            STEP 2: INITIALIZE arr[]= {1, 2, 3, 4, 2, 7, 8, 8, 3}.
+//                    STEP 3: PRINT "Duplicate elements in given array:"
+//            STEP 4: REPEAT STEP 5 to STEP 7 for(i=0; i<arr.length; i++)
+//                STEP 5: REPEAT STEP 6 and STEP 7 for(j=i+1; j<arr.length; j++)
+//                STEP 6: if(arr[i] == arr[j])
+//                STEP 7: PRINT arr[j]
+//            STEP 8: END
 
 
+            int[] arr = {1, 2, 3, 4, 2, 7, 8, 8, 3};
+
+
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i+1; j < arr.length; j++) {
+                    if (arr[i] == arr[j]) {
+                        System.out.println(arr[j]);
+                    }
+                }
+
+            }
+        }
+    }
 
 
 
